7a7b1f38fe652127a290f7c17f7b2db2b08598f6
Jesus Maria Villalobos <jveyes@gmail.com>
2024-01-25 18:16:16 -0500

    test: add unit tests for package service

--- code/static/js/colombia-timezone.js
+++ code/static/js/colombia-timezone.js
@@ -202,3 +202,5 @@ console.log('🕐 Offset Colombia: UTC-5 (sin horario de verano)');
 # Change: 1757423579
 # Commit 20 - 2024-01-19
 # Change: 1757423580
+# Commit 27 - 2024-01-25
+# Change: 1757423580
